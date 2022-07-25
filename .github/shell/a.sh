955d974b52270fd1e724faabccee5665d44e77cf
Bravindrakumar <45438713+Bravindrakumar@users.noreply.github.com>
2022-07-25 16:14:44 +0530

    Update a.sh

--- .github/shell/a.sh
+++ .github/shell/a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-spinup_dir=grep -R --exclude_dir={'zx', 'ab'} /b
+spinup_dir=grep -r --exclude_dir={'zx', 'ab'} /b
 echo "::set-output name=matrix::$(ls b/** | jq -R -s -c 'split("\n")[:-1]')"
