37e182cb228528d4aa0befb6e609f16d06d7041e
Bravindrakumar <45438713+Bravindrakumar@users.noreply.github.com>
2022-07-25 16:06:09 +0530

    Update a.sh

--- .github/shell/a.sh
+++ .github/shell/a.sh
@@ -1,2 +1,2 @@
 spinup_dir=grep -R --exclude_dir={'zx', 'ab'} /b
-echo "::set-output name=matrix::(ls b | jq --raw-input --slurp)"
+echo "::set-output name=matrix::$(ls b/** | jq -R -s -c 'split("\n")[:-1]')"
