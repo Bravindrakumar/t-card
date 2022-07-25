18e6f06fe48bceeb35a33dbc56d81d055293d883
Bravindrakumar <45438713+Bravindrakumar@users.noreply.github.com>
2022-07-26 02:44:46 +0530

    Update determinefolder.py

--- .github/shell/determinefolder.py
+++ .github/shell/determinefolder.py
@@ -1,7 +1,27 @@
-from os import walk, path
-exclude = {'./b/ab', './b/zx'}
-for root, dirs, files in walk('.'):
-  if exclude is not None:
-    dirs[:] = [d for d in dirs if path.join(root, d) not in exclude]
-  for name in files:
-    print path.join(root, name)
+import sys
+import json
+import os
+
+def main():
+  if len(sys.argv) < 2:
+    print("Path to file containing modified paths is required.")
+    exit(1)
+  with open(sys.argv[1]) as input_file:
+    modified_files = json.loads(input_file.read())
+  # compile the list of modified files into unique set of root directories
+  exclude = set(['ab', 'zx'])
+  envs = []
+  directory_set = set([os.path.dirname(file_path) for file_path in modified_files])
+  for directory in directory_set:
+    dirs[:] = [d for d in dirs if d not in exclude]
+    envs.append({ "env":directory })
+  if len(envs) == 0:
+    print("no environment parsed from input file:")
+    print(modified_fils)
+    exit(2)
+    
+  includeMatrix = { "include": envs }
+  print(json.dumps(includeMatrix))
+  
+if __name__ == "__main__":
+  main()
