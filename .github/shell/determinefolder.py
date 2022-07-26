45343c2c0959797c2c51b156dcb65727a59fb4ec
Bravindrakumar <45438713+Bravindrakumar@users.noreply.github.com>
2022-07-26 23:01:38 +0530

    Update determinefolder.py

--- .github/shell/determinefolder.py
+++ .github/shell/determinefolder.py
@@ -10,19 +10,7 @@ def main():
     modified_files = json.loads(input_file.read())
   # compile the list of modified files into unique set of root directories
   
-  b = []
-  exclude = set(['ab', 'zx'])
-  directory_set = set([os.path.dirname(file_path) for file_path in modified_files])
-  for directory in directory_set:
-    if directory == "directory_set":
-    b.append({ "env": directory })
-  if len(b) == 0:
-    print("no environment parsed from input file:")
-    print(modified_fils)
-    exit(2)
-    
-  includeMatrix = { "include": b }
-  print(json.dumps(includeMatrix))
+  
   
 if __name__ == "__main__":
   main()
