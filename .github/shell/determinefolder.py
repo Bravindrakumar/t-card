0bb878aef6d66311eefce3e260a60abeea50e417
Bravindrakumar <45438713+Bravindrakumar@users.noreply.github.com>
2022-08-02 19:07:47 +0530

    Update determinefolder.py

--- .github/shell/determinefolder.py
+++ .github/shell/determinefolder.py
@@ -10,10 +10,14 @@ def main():
   # compile the list of modified files into unique set of root directories
   b = []
   exclude = set(['ab', 'zx'])
-  
+  requirementchanges = sys.argv[1].split('\n')
+  environment = ''
   for dname, dirs, files in os.walk('b'):
     dirs[:] = [d for d in dirs if d not in exclude]
-    b.append({"env": dname})
+    for change in requirement changes:
+      file_name_split_by_forward_slash = change.split('/')
+      environment = file_name_split_by_forward_slash[1]
+    b.append({ "env": environment })
     
   includeMatrix = { "include": b }
   print(json.dumps(includeMatrix))
